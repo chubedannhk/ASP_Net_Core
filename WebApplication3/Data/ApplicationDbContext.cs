31ce2501d843c71dad9084cf03d999b409f087cb
Nguyen Hoang Khai <nguyenhoangkhai2003tgl@gmail.com>
2023-03-01 15:35:10 +0700

    complete WebApplication3

--- WebApplication3/Data/ApplicationDbContext.cs
+++ WebApplication3/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
 
 namespace WebApplication3.Data;
 public class ApplicationDbContext : IdentityDbContext
@@ -8,4 +9,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<WebApplication3.Models.Joke> Joke { get; set; } = default!;
 }
---
+++ WebApplication3/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication3.Models;
+
+public class Joke
+{
+    //phím tắt: gõ prop + 2tab
+    public int Id { get; set; }
+    public string JokeQuestion { get; set; }
+    public string JokeAnswer { get; set; }
+  //phím tắt:  gõ ctor + tab
+    public Joke()
+    {
+        
+    }
+}
